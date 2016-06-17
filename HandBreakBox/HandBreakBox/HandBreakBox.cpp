019baeaf2dd51a7b1d07d6e6be5301425671a86f
jay <jay@JAYHOWARD71CE>
2016-06-17 01:41:04 -0700

    Added boost dependency and implemented get file list method.

--- HandBreakBox/HandBreakBox/HandBreakBox.cpp
+++ HandBreakBox/HandBreakBox/HandBreakBox.cpp
@@ -6,33 +6,27 @@
 #include<string>
 #include<Windows.h>
 #include<shellapi.h>
+#include<boost/filesystem.hpp>
+#include <boost/range/iterator_range.hpp>
+#include<iostream>
 
-using namespace std;
 
-bool find_file(const path & dir_path,         // in this directory,
-	const std::string & file_name, // search for this name,
-	path & path_found)            // placing path here if found
-{
-	if (!exists(dir_path)) return false;
-	directory_iterator end_itr; // default construction yields past-the-end
-	for (directory_iterator itr(dir_path);
-		itr != end_itr;
-		++itr)
-	{
-		if (is_directory(itr->status()))
-		{
-			if (find_file(itr->path(), file_name, path_found)) return true;
-		}
-		else if (itr->leaf() == file_name) // see below
-		{
-			path_found = itr->path();
-			return true;
-		}
+//recursively get files in main directory
+//Add video file locations to queue
+//Convert Files
+//If queue is empty, move all files out.
+
+using namespace boost::filesystem;
+void getFileList(){
+	if (is_directory(p)) {
+		std::cout << p << " is a directory containing:\n";
+
+		for (auto& entry : boost::make_iterator_range(directory_iterator(p), {}))
+			std::cout << entry << "\n";
 	}
-	return false;
 }
 
-
+using namespace std;
 void callHandBrakeCLI(string inFile, string outFile = "%HOMEPATH%\\Videos", string flags = "--preset = \"Normal\"") {
 
 	//Generates the encode flags to be used by ShellExecute
@@ -76,6 +70,8 @@ bool find_file(const path & dir_path,         // in this directory,
 	return false;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+	path p(argc>1 ? argv[1] : ".");
 
+	
 }
